893c0cc945e1d0d7a258d0d4c0ad2feacbdf6824
Ondřej Švorc <ondrejsvorc@email.cz>
2025-10-26 13:51:36 +0100

    Prepare timesheet readers for implementation

---
+++ backend/Timesheets.Api/Timesheets/TimesheetReaders.cs
@@ -0,0 +1,22 @@
+﻿namespace Timesheets.Api.Timesheets;
+
+public interface ITimesheetReader<T>
+{
+    T Read(Stream stream);
+}
+
+public sealed class AttendanceTimesheetReader : ITimesheetReader<AttendanceTimesheet>
+{
+    public AttendanceTimesheet Read(Stream stream)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public sealed class ProjectTimesheetReader : ITimesheetReader<ProjectTimesheet>
+{
+    public ProjectTimesheet Read(Stream stream)
+    {
+        throw new NotImplementedException();
+    }
+}
