d3a65b1e505386a4eb6b0fc07d8a61360b68dea4
Ondřej Švorc <ondrejsvorc@email.cz>
2025-10-27 13:55:24 +0100

    Remove default React app and assets

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+
+export const App = () => {
+  return <p>App is running</p>
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
