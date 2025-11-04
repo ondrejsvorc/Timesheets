c800244b633ccc6b23be1a9ff02fc9235d16c03b
ValdemarPospisil <valda.pospisil.02@gmail.com>
2025-11-04 15:31:58 +0100

    Přidání nového souboru ImportProjectTimesheet.

---
+++ backend/Timesheets.Api/Timesheets/Endpoints/ImportProjectTimesheet.cs
@@ -0,0 +1,36 @@
+using FluentValidation;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Timesheets.Api.Common.Extensions;
+
+namespace Timesheets.Api.Timesheets.Endpoints;
+
+public sealed class ImportProjectTimesheet : IEndpoint
+{
+    public static void Map(IEndpointRouteBuilder app) => app
+        .MapPost("/project/import", Handle)
+        .WithSummary("Importuje výkaz projektové činnosti.")
+        .DisableAntiforgery()
+        .WithRequestValidation<Request>();
+
+    public sealed record Request(IFormFile File);
+    public sealed record Response(ProjectTimesheet Timesheet);
+    public sealed class RequestValidator : AbstractValidator<Request>
+    {
+        public RequestValidator()
+        {
+            RuleFor(x => x.File)
+                .NotNull().WithMessage("Soubor je povinný.")
+                .Must(file => Path.GetExtension(file.FileName).ToLowerInvariant() is ".xls" or ".xlsx")
+                .WithMessage("Soubor musí být ve formátu .xls nebo .xlsx.");
+        }
+    }
+
+    private static async Task<Results<Ok<Response>, BadRequest<string>>> Handle([FromForm] Request request, [FromServices] ITimesheetImporter<ProjectTimesheet> importer)
+    {
+        await using var stream = request.File.OpenReadStream();
+        ProjectTimesheet timesheet = await importer.ImportAsync(stream);
+        Response response = new(timesheet);
+        return TypedResults.Ok(response);
+    }
+}
