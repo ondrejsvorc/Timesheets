678019dfdba3f630ac934153185b4f4e66d32601
Ondřej Švorc <ondrejsvorc@email.cz>
2025-11-14 00:15:33 +0100

    Register prepared endpoints

--- backend/Timesheets.Api/Endpoints.cs
+++ backend/Timesheets.Api/Endpoints.cs
@@ -1,4 +1,7 @@
-﻿using Timesheets.Api.Timesheets.Endpoints;
+﻿using Timesheets.Api.Contracts.Endpoints;
+using Timesheets.Api.Employees.Endpoints;
+using Timesheets.Api.Projects.Endpoints;
+using Timesheets.Api.Timesheets.Endpoints;
 
 namespace Timesheets.Api;
 
@@ -11,19 +14,45 @@ public static class Endpoints
 {
     public static void MapEndpoints(this WebApplication app)
     {
-        var endpoints = app.MapGroup("/api").WithOpenApi();
+        var endpoints = app.MapGroup("/api");
+        endpoints.MapProjectEndpoints();
+        endpoints.MapContractEndpoints();
         endpoints.MapTimesheetEndpoints();
+        endpoints.MapEmployeesEndpoints();
     }
 
-    private static void MapTimesheetEndpoints(this IEndpointRouteBuilder app)
-    {
-        var endpoints = app.MapGroup("/timesheets")
-            .WithTags("Timesheets");
+    private static void MapProjectEndpoints(this IEndpointRouteBuilder app) =>
+        app.MapGroup("/projects").WithTags("Projects")
+        .MapEndpoint<CreateProject>()
+        .MapEndpoint<CreateProjectContract>()
+        .MapEndpoint<DeleteProject>()
+        .MapEndpoint<GetProject>()
+        .MapEndpoint<GetProjectContracts>()
+        .MapEndpoint<GetProjects>()
+        .MapEndpoint<UpdateProject>();
 
-        endpoints
-            .MapEndpoint<ImportAttendanceTimesheet>()
-            .MapEndpoint<ImportProjectTimesheet>();
-    }
+    private static void MapContractEndpoints(this IEndpointRouteBuilder app) =>
+        app.MapGroup("/contracts").WithTags("Contracts")
+        .MapEndpoint<GetContract>()
+        .MapEndpoint<UpdateContract>()
+        .MapEndpoint<DeleteContract>()
+        .MapEndpoint<GetContractEmployees>()
+        .MapEndpoint<AddContractEmployee>()
+        .MapEndpoint<UpdateContractEmployee>()
+        .MapEndpoint<RemoveContractEmployee>();
+
+    private static void MapTimesheetEndpoints(this IEndpointRouteBuilder app) =>
+        app.MapGroup("/timesheets").WithTags("Timesheets")
+        .MapEndpoint<ImportAttendanceTimesheet>()
+        .MapEndpoint<ImportProjectTimesheet>();
+
+    private static void MapEmployeesEndpoints(this IEndpointRouteBuilder app) =>
+        app.MapGroup("/employees").WithTags("Employees")
+        .MapEndpoint<CreateEmployee>()
+        .MapEndpoint<DeleteEmployee>()
+        .MapEndpoint<GetEmployee>()
+        .MapEndpoint<GetEmployees>()
+        .MapEndpoint<UpdateEmployee>();
 
     private static IEndpointRouteBuilder MapEndpoint<TEndpoint>(this IEndpointRouteBuilder app) where TEndpoint : IEndpoint
     {
