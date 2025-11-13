625ce2b4d911d9e0a4114e18b7696320bc270daf
Ondřej Švorc <ondrejsvorc@email.cz>
2025-11-14 00:15:17 +0100

    Prepare employee endpoints

---
+++ backend/Timesheets.Api/Employees/Endpoints/DeleteEmployee.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Timesheets.Api.Common.Extensions;
+
+namespace Timesheets.Api.Employees.Endpoints;
+
+public sealed class DeleteEmployee : IEndpoint
+{
+    public static void Map(IEndpointRouteBuilder app) =>
+        app.MapDelete("/{id}", Handle)
+           .WithSummary("Delete Employee")
+           .WithRequestValidation<Request>();
+
+    public sealed record Request;
+    public sealed record Response;
+    public sealed class Validator : AbstractValidator<Request> { }
+
+    private static async Task<Results<NoContent, NotFound>> Handle(Guid id, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ backend/Timesheets.Api/Employees/Endpoints/GetEmployees.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Timesheets.Api.Common.Extensions;
+
+namespace Timesheets.Api.Employees.Endpoints;
+
+public sealed class GetEmployees : IEndpoint
+{
+    public static void Map(IEndpointRouteBuilder app) =>
+        app.MapGet("/", Handle)
+           .WithSummary("Get Employees")
+           .WithRequestValidation<Request>();
+
+    public sealed record Request;
+    public sealed record Response;
+    public sealed class Validator : AbstractValidator<Request> { }
+
+    private static async Task<Ok<Response>> Handle(CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
