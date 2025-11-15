3d9578964c9d156e3db7c112ffe225accc61e013
Ondřej Švorc <ondrejsvorc@email.cz>
2025-11-15 18:29:06 +0100

    Create AppDbContext.cs

---
+++ backend/Timesheets.Api/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Timesheets.Api.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext()
+    {
+        throw new NotImplementedException();
+    }
+}
