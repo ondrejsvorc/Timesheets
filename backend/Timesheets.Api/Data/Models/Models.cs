5efe36801bbd58e80a89418b5c1962e5780deb5e
Ondřej Švorc <ondrejsvorc@email.cz>
2025-11-23 13:20:17 +0100

    Add Name and RegistrationNumber to Contract model

--- backend/Timesheets.Api/Data/Models/Models.cs
+++ backend/Timesheets.Api/Data/Models/Models.cs
@@ -27,6 +27,8 @@ public sealed class Contract
 {
     public Guid Id { get; set; }
     public Guid ProjectId { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string? RegistrationNumber { get; set; }
     public DateTime StartDate { get; set; }
     public DateTime? EndDate { get; set; }
     public string? Description { get; set; }
