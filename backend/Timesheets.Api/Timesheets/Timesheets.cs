c139fe45876d35b7987510a442f73cee87ce664e
Ondřej Švorc <ondrejsvorc@email.cz>
2025-11-29 13:28:32 +0100

    Change CombinedDay Date from DateOnly to DateTime

--- backend/Timesheets.Api/Timesheets/Timesheets.cs
+++ backend/Timesheets.Api/Timesheets/Timesheets.cs
@@ -35,7 +35,7 @@ public sealed record CombinedTimesheet(
 }
 
 public sealed record CombinedDay(
-    DateOnly Date,
+    DateTime Date,
     bool IsHoliday,
     bool IsWeekend,
     bool IsWorkday,
