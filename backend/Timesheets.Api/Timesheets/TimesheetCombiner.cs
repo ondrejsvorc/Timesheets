3a583c21a55a4687124574cf04656a58a5926479
Ondřej Švorc <ondrejsvorc@email.cz>
2025-12-09 16:32:51 +0100

    Change CombineDays return type to List<CombinedDay>

--- backend/Timesheets.Api/Timesheets/TimesheetCombiner.cs
+++ backend/Timesheets.Api/Timesheets/TimesheetCombiner.cs
@@ -13,26 +13,25 @@ public class TimesheetCombiner : ITimesheetCombiner
         return new CombinedTimesheet(attendance.Year, attendance.Month, days);
     }
 
-    private static IReadOnlyList<CombinedDay> CombineDays(AttendanceTimesheet attendance, IReadOnlyList<ProjectTimesheet> projects)
+    private static List<CombinedDay> CombineDays(AttendanceTimesheet attendance, IReadOnlyList<ProjectTimesheet> projects)
     {
-        return attendance.Days
-            .Select((attendanceDay, index) =>
-            {
-                List<ProjectDay> projectDays = projects
-                    .Select(p => p.Days[index])
-                    .ToList();
+        return attendance.Days.Select((attendanceDay, index) =>
+        {
+            List<ProjectDay> projectDays = projects
+                .Select(p => p.Days[index])
+                .ToList();
 
-                return new CombinedDay(
-                    Date: attendanceDay.Date,
-                    IsHoliday: attendanceDay.IsHoliday,
-                    IsWeekend: attendanceDay.IsWeekend,
-                    IsWorkday: attendanceDay.IsWorkday,
-                    AttendanceHours: attendanceDay.TotalHours,
-                    ProjectHours: projectDays.Sum(d => d.Hours),
-                    AttendanceWorkload: attendanceDay.Workload,
-                    ProjectWorkload: projectDays.Sum(d => d.Workload)
-                );
-            })
-            .ToList();
+            return new CombinedDay(
+                Date: attendanceDay.Date,
+                IsHoliday: attendanceDay.IsHoliday,
+                IsWeekend: attendanceDay.IsWeekend,
+                IsWorkday: attendanceDay.IsWorkday,
+                AttendanceHours: attendanceDay.TotalHours,
+                ProjectHours: projectDays.Sum(d => d.Hours),
+                AttendanceWorkload: attendanceDay.Workload,
+                ProjectWorkload: projectDays.Sum(d => d.Workload)
+            );
+        })
+        .ToList();
     }
 }
