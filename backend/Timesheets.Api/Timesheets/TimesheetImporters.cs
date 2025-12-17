b146b8762a75f551672bb21b159053d1dd7bbede
Ondřej Švorc <ondrejsvorc@email.cz>
2025-12-17 13:33:50 +0100

    Replace dependency on external public holidays API with my NuGet package CzechHolidays

--- backend/Timesheets.Api/Timesheets/TimesheetImporters.cs
+++ backend/Timesheets.Api/Timesheets/TimesheetImporters.cs
@@ -1,42 +1,40 @@
-﻿namespace Timesheets.Api.Timesheets;
+﻿using CzechHolidays;
+
+namespace Timesheets.Api.Timesheets;
 
 public interface ITimesheetImporter<T> where T : ITimesheet
 {
     Task<T> ImportAsync(Stream stream);
 }
 
-public sealed class AttendanceTimesheetImporter(ITimesheetReader<AttendanceTimesheet> reader, IPublicHolidayProvider provider) : ITimesheetImporter<AttendanceTimesheet>
+public sealed class AttendanceTimesheetImporter(ITimesheetReader<AttendanceTimesheet> reader, ICzechHolidaysFactory factory) : ITimesheetImporter<AttendanceTimesheet>
 {
     public async Task<AttendanceTimesheet> ImportAsync(Stream stream)
     {
         AttendanceTimesheet timesheet = reader.Read(stream);
-
-        IReadOnlyCollection<PublicHoliday> holidays = await provider.GetPublicHolidaysAsync(timesheet.Year);
-        HashSet<DateTime> holidayDates = holidays.Select(h => h.Date).ToHashSet();
+        CzechHolidaysYear holidays = factory.Create(timesheet.Year);
 
         return timesheet with
         {
             Days = timesheet.Days
-                .Select(day => day with { IsHoliday = holidayDates.Contains(day.Date) })
+                .Select(day => day with { IsHoliday = holidays.Contains(day.Date) })
                 .ToList()
         };
     }
 }
 
 [Obsolete]
-public sealed class ProjectTimesheetImporter(ITimesheetReader<ProjectTimesheet> reader, IPublicHolidayProvider provider) : ITimesheetImporter<ProjectTimesheet>
+public sealed class ProjectTimesheetImporter(ITimesheetReader<ProjectTimesheet> reader, ICzechHolidaysFactory factory) : ITimesheetImporter<ProjectTimesheet>
 {
     public async Task<ProjectTimesheet> ImportAsync(Stream stream)
     {
         ProjectTimesheet timesheet = reader.Read(stream);
-
-        IReadOnlyCollection<PublicHoliday> holidays = await provider.GetPublicHolidaysAsync(timesheet.Year);
-        HashSet<DateTime> holidayDates = holidays.Select(h => h.Date).ToHashSet();
+        CzechHolidaysYear holidays = factory.Create(timesheet.Year);
 
         return timesheet with
         {
             Days = timesheet.Days
-                .Select(day => day with { IsHoliday = holidayDates.Contains(day.Date) })
+                .Select(day => day with { IsHoliday = holidays.Contains(day.Date) })
                 .ToList()
         };
     }
