4634d97c0c2cefa74bcba2579efc8d7a7c3d7848
ondrejsvorc <ondrejsvorc@email.cz>
2026-01-01 22:48:55 +0100

    Add clarification comment for cn utility

--- poc/src/utils/cn.ts
+++ poc/src/utils/cn.ts
@@ -1,6 +1,11 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+/** Merges Tailwind classes using clsx for conditional logic, then twMerge to resolve conflicts. 
+ * Used by Shadcn components to merge classes.
+ * @param inputs - Tailwind classes to merge. 
+ * @returns Merged Tailwind classes.
+*/
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
