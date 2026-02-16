ff6258243ec18490296ebef19ad6cd86906229c2
Ondřej Švorc <ondrejsvorc@email.cz>
2026-02-16 14:18:19 +0100

    Remove old frontend project

--- frontend/src/common/Constants.ts
+++
@@ -1,5 +0,0 @@
-const isDev = import.meta.env.DEV;
-
-export const Constants = {
-  apiUrl: isDev ? "https://localhost:7096/api" : "insert production url here"
-} as const;
--- frontend/src/projects/api/deleteProject.ts
+++
@@ -1,9 +0,0 @@
-import { Constants } from "../../common/Constants";
-
-export const deleteProject = async (id: string): Promise<void> => {
-  const response = await fetch(`${Constants.apiUrl}/projects/${id}`, {
-    method: "DELETE",
-  });
-  if (!response.ok) throw new Error("Failed to delete project.");
-};
-
