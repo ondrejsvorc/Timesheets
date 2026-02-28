924e4443bf3dbb4dad0fc6092782865d27b25a09
ondrejsvorc <ondrejsvorc@email.cz>
2026-02-28 15:34:50 +0100

    Add GetProjectContract endpoint

---
+++ backend/Timesheets.Api/Projects/Endpoints/GetProjectContract.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.CodeAnalysis;
+using Microsoft.EntityFrameworkCore;
+using Timesheets.Api.Data;
+
+namespace Timesheets.Api.Projects.Endpoints;
+
+public sealed class GetProjectContract : IEndpoint
+{
+    public static void Map(IEndpointRouteBuilder app) =>
+        app.MapGet("/{projectId}/contracts/{contractId}", Handle)
+           .WithSummary("Get Project Contract");
+
+    public sealed record Response(Guid Id, string Name, string RegistrationNumber);
+
+    private static async Task<Results<Ok<Response>, NotFound>> Handle(Guid projectId, Guid contractId, AppDbContext dbContext, CancellationToken cancellationToken)
+    {
+        Response? contract = await dbContext.Contracts
+            .AsNoTracking()
+            .Where(c => c.ProjectId == projectId && c.Id == contractId)
+            .Select(c => new Response(
+                c.Id,
+                c.Name,
+                c.RegistrationNumber
+            ))
+            .FirstOrDefaultAsync(cancellationToken);
+
+        if (contract is null)
+        {
+            return TypedResults.NotFound();
+        }
+
+        return TypedResults.Ok(contract);
+    }
+}
