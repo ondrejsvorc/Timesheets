adba8a7da267ffb98588c298347d1a352618b2af
ondrejsvorc <ondrejsvorc@email.cz>
2026-03-21 14:24:06 +0100

    Improve loading skeleton by adding padding

--- frontend/src/components/shared/data/GenericSkeleton.tsx
+++ frontend/src/components/shared/data/GenericSkeleton.tsx
@@ -1,7 +1,7 @@
 import { Skeleton } from "@/components/ui/skeleton";
 
 export const GenericSkeleton = () => (
-  <div className="w-full space-y-3">
+  <div className="w-full space-y-3 py-3">
     <Skeleton className="h-5 w-2/5 [animation-duration:0.5s]" />
     <Skeleton className="h-4 w-full [animation-duration:0.5s]" />
     <Skeleton className="h-4 w-5/6 [animation-duration:0.5s]" />
